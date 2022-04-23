a3b6f070cdcbcee393012ce60fdee004c133749f
G9host <85241527+G9host@users.noreply.github.com>
2022-04-23 15:19:41 +0530

    All Game Essentials added
    All Game Essentials added -
    1. Build Report
    2. DoTween Pro
    3. Toony Colors Pro v2.6
    4. Myscripts

---
+++ Assets/Empty_GameProjectName/Scripts/MyScripts/GameEssentials.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DDZ
+{
+    public class GameEssentials : Singleton<GameEssentials>
+    {
+        public static string remoteConfigVal = "0";
+
+        public SavedData sd;
+        public SceneLoader sl;
+        public SoundHapticManager shm;
+
+        void Start()
+        {
+            //GameObject.Find("EventSystem").SetActive(false);
+        }
+
+        public void PrintOut(string val)
+        {
+            print(val);
+        }
+
+    }
+}
+
